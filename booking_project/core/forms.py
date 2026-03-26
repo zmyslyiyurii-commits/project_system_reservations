8a005c9c5150ec921528c033c61978c24abb1c97
Yura <zmyslyi.yurii@gmail.com>
2026-03-26 19:54:05 +0200

    Виконано Пункт 2 ТЗ: Створено форму бронювання та шаблони

--- booking_project/core/forms.py
+++ booking_project/core/forms.py
@@ -6,7 +6,12 @@ class BookingForm(forms.ModelForm):
         model = Booking
         fields = ['room', 'start_date', 'end_date']
         widgets = {
-            'start_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'end_date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control'}),
-            'room': forms.Select(attrs={'class': 'form-control'}),
+            'room': forms.Select(attrs={'class': 'form-select'}),
+            'start_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+            'end_date': forms.DateInput(attrs={'class': 'form-control', 'type': 'date'}),
+        }
+        labels = {
+            'room': 'Оберіть кімнату',
+            'start_date': 'Дата початку',
+            'end_date': 'Дата завершення',
         }
--- booking_project/core/views.py
+++ booking_project/core/views.py
@@ -4,7 +4,7 @@ from .models import Room, Booking
 from .forms import BookingForm
 
 def room_list(request):
-    rooms = Room.objects.all() 
+    rooms = Room.objects.all()
     return render(request, 'core/room_list.html', {'rooms': rooms})
 
 @login_required
@@ -13,9 +13,10 @@ def book_room(request):
         form = BookingForm(request.POST)
         if form.is_valid():
             booking = form.save(commit=False)
-            booking.user = request.user
+            booking.user = request.user  # Прив'язуємо бронювання до того, хто увійшов
             booking.save()
             return redirect('room_list')
     else:
         form = BookingForm()
+    
     return render(request, 'core/booking_form.html', {'form': form})
